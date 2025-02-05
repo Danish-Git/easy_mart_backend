849f67eebc4280d7b59efe152e0aebdab042e8a3
Danish Ahmad Wasim <79436114+Danish-Git@users.noreply.github.com>
2025-02-05 14:58:49 +0530

    fixes

--- otp/views.py
+++ otp/views.py
@@ -1,6 +1,9 @@
 from django.http import JsonResponse
 from django.views import View
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 
+@method_decorator(csrf_exempt, name='dispatch')
 class SendOtpView(View):
     def get(self, request):
         return JsonResponse({"message": "Hello World"})
