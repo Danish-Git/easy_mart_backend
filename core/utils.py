6b95d86aadb69772ea8c35b6264a0c2437f627b7
Danish Ahmad Wasim <79436114+Danish-Git@users.noreply.github.com>
2025-02-06 11:14:24 +0530

    Fixed send OTP and Verify OTP APIs

--- core/utils.py
+++ core/utils.py
@@ -1,28 +1,29 @@
-# import requests
-
-# FAST2SMS_API_KEY = "qLyM3N5GPJ7xlR2gnvzXHiQBDcU8Wkhd9I4wSsjAaeKtZV1OfrlJVoE09nD5c3YCmjSNd1UFXK2hxs8H"  # Replace with your Fast2SMS API key
-
-# def send_otp_via_fast2sms(phone_number, otp):
-#     url = "https://www.fast2sms.com/dev/bulkV2"
-#     payload = {
-#         "variables_values" : "{otp}",
-#         "route": "otp", 
-#         "numbers": phone_number
-#     }
-#     headers = {
-#         "authorization": FAST2SMS_API_KEY,
-#         "Content-Type":"application/json"
-#     }
-
-#     response = requests.post(url, data=payload, headers=headers)
-#     return response.json()  # Return the response as a dictionary
-
-
 import requests
 from django.http import JsonResponse
 
+FAST2SMS_API_KEY = "qLyM3N5GPJ7xlR2gnvzXHiQBDcU8Wkhd9I4wSsjAaeKtZV1OfrlJVoE09nD5c3YCmjSNd1UFXK2hxs8H"  # Replace with your Fast2SMS API key
+
 FAZPASS_API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZGVudGlmaWVyIjo4NTk1fQ.JfuFnkd_e7MS1eijpDlmb9REo4jiP80IejCQMcJaoEc"  # Replace with your Fazpass API key
 GATEWAY_KEY = "549969fd-f1e8-486c-a2b7-8fda60a195a0"  # Replace with your Gateway Key
+
+#########################     Send OTP Via Fast2Sms     #########################
+
+def send_otp_via_fast2sms(phone_number, otp):
+    url = "https://www.fast2sms.com/dev/bulkV2"
+    payload = {
+        "variables_values" : "{otp}",
+        "route": "otp", 
+        "numbers": phone_number
+    }
+    headers = {
+        "authorization": FAST2SMS_API_KEY,
+        "Content-Type":"application/json"
+    }
+
+    response = requests.post(url, data=payload, headers=headers)
+    return response.json()  # Return the response as a dictionary
+
+#########################   Send OTP Via Fazpass     #########################
 
 def send_otp_via_fazpass(phone_number):
     url = "https://api.fazpass.com/v1/otp/request"
@@ -44,6 +45,8 @@ def send_otp_via_fazpass(phone_number):
     response = requests.post(url, json=payload, headers=headers)
     return response.json()  # Return the response as a dictionary
 
+#########################     Verify OTP Via Fazpass     #########################
+
 def verify_otp_via_fazpass(otp_id, otp):
     url = "https://api.fazpass.com/v1/otp/verify"
     payload = {
@@ -54,6 +57,6 @@ def verify_otp_via_fazpass(otp_id, otp):
         "Authorization": f"Bearer {FAZPASS_API_KEY}",
         "Content-Type": "application/json"
     }
-    return JsonResponse({"message": "OTP verified successfully", "status":200}, status=200, safe=True)
-    # response = requests.post(url, json=payload, headers=headers)
-    # return response.json()  # Return the response as a dictionary
+    # return JsonResponse({"message": "OTP verified successfully", "status":200}, status=200, safe=True)
+    response = requests.post(url, json=payload, headers=headers)
+    return response.json()  # Return the response as a dictionary
--- otp/views.py
+++ otp/views.py
@@ -1,4 +1,3 @@
-import random
 import jwt
 import json
 from django.conf import settings
@@ -9,7 +8,8 @@ from django.views.decorators.csrf import csrf_exempt
 from core.db_operations.user import save_user, get_user_by_phone, phone_exists, update_user
 from core.utils import send_otp_via_fazpass
 from core.utils import verify_otp_via_fazpass
-from datetime import datetime
+
+###########################     Send OTP     ###########################
 
 @method_decorator(csrf_exempt, name='dispatch')
 class SendOtpView(View):
@@ -45,74 +45,8 @@ class SendOtpView(View):
                 "purpose": fazpass_response.get("data", {}).get("purpose")
             }
         })
-    
 
-# @method_decorator(csrf_exempt, name='dispatch')
-# class VerifyOtpView(View):
-#     def post(self, request):
-#         phone_no = request.POST.get('phone_no')
-#         otp_id = request.POST.get('otp_id')
-#         otp = request.POST.get('otp')
-
-#         if not phone_no or not otp:
-#             return JsonResponse({"error": "Phone number and OTP are required"}, status=400)
-
-#         user = get_user_by_phone(phone_no, otp_id)
-
-#         # Verify OTP via Fazpass
-#         fazpass_response = verify_otp_via_fazpass(otp_id, otp)
-
-#         if not fazpass_response.get("status"):
-#             return JsonResponse({"error": "Failed to verify OTP via Fazpass"}, status=500)
-
-#         if not user:
-#             return JsonResponse({"error": "User not found"}, status=404)
-
-#         if user.otp == otp:
-#             # Update the user to mark OTP as verified
-#             update_user(phone_number, otp, is_verified=True)
-#             return JsonResponse({"message": "OTP verified successfully"})
-#         else:
-#             return JsonResponse({"error": "Invalid OTP"}, status=400)
-
-# @method_decorator(csrf_exempt, name='dispatch')
-# class VerifyOtpView(View):
-#     def post(self, request):
-#         try:
-#             data = json.loads(request.body)  # Use JSON request body
-#         except json.JSONDecodeError:
-#             return JsonResponse({"error": "Invalid JSON"}, status=400)
-
-#         phone_no = data.get('phone_no')
-#         otp_id = data.get('otp_id')
-#         otp = data.get('otp')
-
-#         if not phone_no or not otp or not otp_id:
-#             return JsonResponse({"error": "phone_no, otp_id, and otp are required"}, status=400)
-
-#         user = get_user_by_phone(phone_no)  # Fetch user
-
-#         # Verify OTP via Fazpass
-#         fazpass_response = verify_otp_via_fazpass(otp_id, otp)
-
-#         # Return full response from Fazpass
-#         if not fazpass_response.get("status"):
-#             fazpass_response_data = fazpass_response.content
-#             fazpass_response = {"response": fazpass_response_data} 
-#             return JsonResponse(fazpass_response, status=400)
-
-#         if not user:
-#             return JsonResponse({"error": "User not found"}, status=404)
-
-#         # If OTP is correct, generate JWT token
-#         token = generate_jwt_token(user)
-
-#         # Update user verification status
-#         update_user(phone_no, otp, is_verified=True)
-
-#         # Include token in the response
-#         fazpass_response["jwt_token"] = token
-#         return JsonResponse(fazpass_response)
+###########################     Verify OTP     ###########################
 
 @method_decorator(csrf_exempt, name='dispatch')
 class VerifyOtpView(View):
@@ -155,6 +89,8 @@ class VerifyOtpView(View):
         # Include token in the response
         fazpass_response_data["token"] = token
         return JsonResponse(fazpass_response_data)
+    
+###########################     Generate JWT Token     ###########################
 
 def generate_jwt_token(user):
     """Generate a JWT token for the authenticated user"""
