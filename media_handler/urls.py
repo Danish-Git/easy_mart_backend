e4a13b931f0e0da57f347db8fceb140d8790c310
Danish Ahmad Wasim <79436114+Danish-Git@users.noreply.github.com>
2025-02-06 16:31:13 +0530

    media

--- media_handler/urls.py
+++ media_handler/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload-photo/', views.upload_photo.as_view(), name='upload_photo'),
+    path('upload-photo/', views.ImageUploadView.as_view(), name='upload_media'),
 ]
