31438305811d4b9958b2bbbe0debd661790e0a16
Danish Ahmad Wasim <79436114+Danish-Git@users.noreply.github.com>
2025-02-06 16:46:06 +0530

    Update urls.py

--- easy_mart_backend/urls.py
+++ easy_mart_backend/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,5 +23,13 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('otp.urls')),
     path('api/v1/', include('media_handler.urls')),
+    # path('api/v1/', include('uploads.urls')),
     # path('', include('otp.urls')),
+    # Your other URL patterns for the API and other views
+    path('api/v1/', include('easy_mart_backend.urls')),  # Replace 'your_app' with the actual app name
+    # Any other URLs you may have
 ]
+
+# Add this part to serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
