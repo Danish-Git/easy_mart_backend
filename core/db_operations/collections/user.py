fb8ab9181d324b148654cdd09909bfcfc8a9d312
Danish Ahmad Wasim <79436114+Danish-Git@users.noreply.github.com>
2025-02-07 10:48:18 +0530

    Update user.py

--- core/db_operations/collections/user.py
+++ core/db_operations/collections/user.py
@@ -1,17 +1,13 @@
 import mongoengine as me
 from datetime import datetime
-from .address import Address
-from .media_collection import Media
 
 class User(me.Document):
     phone = me.StringField(unique=True, required=True)
     otp = me.StringField()
     first_name = me.StringField(required=True)
     last_name = me.StringField()
-    primary_address = me.ReferenceField("Address", null=True)
-    profile_photo = me.ReferenceField("Media", null=True)
-    # primary_address = me.ReferenceField(Address, null=True)  # Assuming Address model exists
-    # profile_photo = me.ReferenceField(Media, null=True)  # For storing profile photo ID
+    primary_address = me.ReferenceField("Address", null=True)  # Assuming Address model exists
+    profile_photo = me.ReferenceField("Media", null=True)  # For storing profile photo ID
     profile_photo_url = me.StringField()  # For storing profile photo URL
     created_at = me.DateTimeField(default=datetime.utcnow)
     is_verified = me.BooleanField(default=False)
