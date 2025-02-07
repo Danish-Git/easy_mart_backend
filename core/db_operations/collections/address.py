3515be29d6c95957910df8de1b293e76e9a248bb
Danish Ahmad Wasim <79436114+Danish-Git@users.noreply.github.com>
2025-02-07 10:47:16 +0530

    ReferenceField

--- core/db_operations/collections/address.py
+++ core/db_operations/collections/address.py
@@ -2,19 +2,12 @@ import mongoengine as me
 from datetime import datetime
 
 class Address(me.Document):
-
-    @staticmethod
-    def get_user_reference():
-        from core.db_operations.collections.user import User  # Import inside method
-        return me.ReferenceField(User, null=True)
-    
     address_line1 = me.StringField()
     address_line2 = me.StringField()
     city = me.StringField()
     state = me.StringField()
     country = me.StringField()
     postal_code = me.StringField()
-    # user = get_user_reference()
     user = me.ReferenceField("User", null=True)
     created_at = me.DateTimeField(default=datetime.utcnow)
     updated_at = me.DateTimeField(default=datetime.utcnow)
--- core/db_operations/collections/media_collection.py
+++ core/db_operations/collections/media_collection.py
@@ -6,7 +6,7 @@ class Media(me.Document):
     category = me.StringField(unique=True, required=True)
     image_name = me.StringField()
     image_url = me.StringField(required=True)
-    user = me.ReferenceField(User, null=True)
+    user = me.ReferenceField("User", null=True)
     updated_at = me.DateTimeField(default=datetime.utcnow)
 
     meta = {
