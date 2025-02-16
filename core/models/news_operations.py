351d7aa2156a9e68c86ec2525706a86df4249f0c
Danish Ahmad Wasim <danishwasim1995@gmail.com>
2025-02-16 13:21:55 +0530

    undo changes

--- core/models/news_operations.py
+++ core/models/news_operations.py
@@ -2,20 +2,19 @@ from datetime import datetime
 from core.db_operations.collections.news import News
 from core.db_operations.collections.media_collection import Media
 from core.db_operations.collections.news_categories import NewsCategories
-# from core.models.user import get_user_by_id
-from core.models.user import User
+from core.models.user import get_user_by_id
 
 # Function to create a new news article
-def create_news(posted_by: User, title: str, description: str = None, cover_image: str = None, priority: int = 0,
+def create_news(posted_by: str, title: str, description: str = None, cover_image: str = None, priority: int = 0,
     status: bool = True, is_featured: bool = False, is_trending: bool = False, keywords: list = None,
     language: str = "en", category: str = None, meta_title: str = None, meta_description: str = None,):
 
-    # user = get_user_by_id(posted_by)
+    user = get_user_by_id(posted_by)
     cover_image_doc = Media.objects(id = cover_image).first() if cover_image else None
     category_doc = NewsCategories.objects(id = category).first() if category else None
 
     news = News(
-        posted_by = User,
+        posted_by = user,
         title = title,
         description = description,
         cover_image = cover_image_doc,
