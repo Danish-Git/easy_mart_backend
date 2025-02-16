79fd8e31d6d3c905f7c564321064f21e4ceb1b34
Danish Ahmad Wasim <danishwasim1995@gmail.com>
2025-02-16 18:05:35 +0530

    Update views.py

--- news/views.py
+++ news/views.py
@@ -6,9 +6,6 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from core.utils import validate_jwt_token  
 from core.models.news_operations import create_news, fetch_news
-from core.models.user import get_user_by_id
-from core.models.media_operations import get_media_by_id
-from core.models.news_categories import get_news_category_by_id
 from core.db_operations.collections.media_collection import Media
 
 @method_decorator(csrf_exempt, name='dispatch')
@@ -85,6 +82,8 @@ class CreateNewsView(View):
                     "image_url": news.cover_image.image_url
                 }
 
+            print(cover_image_data)
+
             # Handle category if it's None
             news_category = None
             if news.category:
@@ -142,12 +141,12 @@ class FetchNewsView(View):
         """Fetch paginated news articles"""
         token = request.headers.get('Authorization')
         if not token:
-            return JsonResponse({"error": "Token is required"}, status = 400)
+            return JsonResponse({"error": "Token is required"}, status=400)
 
         token = token.split(' ')[1]
         user = validate_jwt_token(token)
         if not user:
-            return JsonResponse({"error": "Invalid or expired token"}, status = 401)
+            return JsonResponse({"error": "Invalid or expired token"}, status=401)
 
         language = request.GET.get("language", "en")
         category_id = request.GET.get("category")
@@ -155,97 +154,123 @@ class FetchNewsView(View):
         page_size = int(request.GET.get("page_size", 10))
 
         if not category_id:
-            return JsonResponse({"error": "Category ID is required"}, status = 400)
+            return JsonResponse({"error": "Category ID is required"}, status=400)
 
         try:
             category_id = ObjectId(category_id)
         except:
-            return JsonResponse({"error": "Invalid category ID"}, status = 400)
+            return JsonResponse({"error": "Invalid category ID"}, status=400)
 
         news_list = fetch_news(language, category_id, page, page_size)
 
         formatted_news = []
         for news in news_list:
-
-            # Handle posted_by if it's present
-            user_data = None
-            if "posted_by" in news and news["posted_by"]:
-                user = get_user_by_id(news["posted_by"])
-                if user:
-                    user_data = {
-                        "id": str(user.id),
-                        "phone": user.phone,
-                        "first_name": user.first_name,
-                        "last_name": user.last_name,
-                        "profile_photo": {
-                            "image_id": str(user.profile_photo.id),
-                            "category": user.profile_photo.category,
-                            "image_url": user.profile_photo.image_url
-                        } if user.profile_photo else None,
-                        "profile_photo_url": user.profile_photo_url if user.profile_photo_url else None,
-                        "primary_address": {
-                            "id": str(user.primary_address.id),
-                            "address_line1": user.primary_address.address_line1,
-                            "address_line2": user.primary_address.address_line2,
-                            "city": user.primary_address.city,
-                            "state": user.primary_address.state,
-                            "postal_code": user.primary_address.postal_code
-                        } if user.primary_address else None,
-                        "is_verified": user.is_verified,
-                        "created_at": user.created_at.strftime('%Y-%m-%d %H:%M:%S'),
-                        "updated_at": user.updated_at.strftime('%Y-%m-%d %H:%M:%S')
-                    }
-
             # Format cover image
             cover_image_data = None
-            if "cover_image" in news and news["cover_image"]:
-                cover_image_obj = get_media_by_id(news["cover_image"])  # Fetch from MongoDB
-                if cover_image_obj:
-                    cover_image_data = {
-                        "image_id": str(cover_image_obj["_id"]),
-                        "category": cover_image_obj.get("category"),
-                        "image_name": cover_image_obj.get("image_name"),
-                        "image_url": cover_image_obj.get("image_url"),
-                    }
+            if news.get("cover_image"):
+                cover_image_data = {
+                    "image_id": str(news["cover_image"]),
+                    "category": news["cover_image"].get("category"),
+                    "image_name": news["cover_image"].get("image_name"),
+                    "image_url": news["cover_image"].get("image_url"),
+                }
 
             # Format category
             news_category = None
-            if "category" in news and news["category"]:
-                category_obj = get_news_category_by_id(news["category"]) 
-                if category_obj:
-                    news_category = {
-                        "id": str(category_obj["_id"]),
-                        "title": category_obj.get("title"),
-                        "slug": category_obj.get("slug"),
-                        "description": category_obj.get("description"),
-                        "icon_url": category_obj.get("icon_url"),
-                        "priority": category_obj.get("priority"),
-                        "status": category_obj.get("status"),
-                        "is_featured": category_obj.get("is_featured"),
-                        "is_trending": category_obj.get("is_trending"),
-                        "keywords": category_obj.get("keywords"),
-                        "created_at": category_obj.get("created_at").strftime('%Y-%m-%d %H:%M:%S') if category_obj.get("created_at") else None,
-                        "updated_at": category_obj.get("updated_at").strftime('%Y-%m-%d %H:%M:%S') if category_obj.get("updated_at") else None,
-                    }
-
+            if news.get("category"):
+                news_category = {
+                    "id": str(news["category"]),
+                    "title": news["category"].get("title"),
+                    "slug": news["category"].get("slug"),
+                    "description": news["category"].get("description"),
+                    "icon_url": news["category"].get("icon_url"),
+                    "priority": news["category"].get("priority"),
+                    "status": news["category"].get("status"),
+                    "is_featured": news["category"].get("is_featured"),
+                    "is_trending": news["category"].get("is_trending"),
+                    "keywords": news["category"].get("keywords"),
+                }
 
             formatted_news.append({
-                "id": str(news["_id"]),
-                "posted_by": user_data,
-                "title": news["title"],
-                "description": news["description"],
+                "id": str(news),
+                "title": news.get("title"),
+                "description": news.get("description"),
                 "cover_image": cover_image_data,
-                "priority": news["priority"],
-                "status": news["status"],
-                "is_featured": news["is_featured"],
-                "is_trending": news["is_trending"],
-                "keywords": news["keywords"],
-                "language": news["language"],
+                "priority": news.get("priority", 0),
+                "status": news.get("status", True),
+                "is_featured": news.get("is_featured", False),
+                "is_trending": news.get("is_trending", False),
+                "keywords": news.get("keywords", []),
+                "language": news.get("language"),
                 "category": news_category,
-                "meta_title": news["meta_title"],
-                "meta_description": news["meta_description"],
-                "created_at": news["created_at"].strftime('%Y-%m-%d %H:%M:%S'),
-                "updated_at": news["updated_at"].strftime('%Y-%m-%d %H:%M:%S')
+                "meta_title": news.get("meta_title"),
+                "meta_description": news.get("meta_description"),
+                "created_at": news["created_at"].strftime('%Y-%m-%d %H:%M:%S') if "created_at" in news else None,
+                "updated_at": news["updated_at"].strftime('%Y-%m-%d %H:%M:%S') if "updated_at" in news else None,
             })
 
         return JsonResponse({"message": "News fetched successfully", "data": formatted_news}, status=200)
+    
+
+
+
+    # # Handle posted_by if it's present
+    #         user_data = None
+    #         if "posted_by" in news and news["posted_by"]:
+    #             user = get_user_by_id(news["posted_by"])
+    #             if user:
+    #                 user_data = {
+    #                     "id": str(user.id),
+    #                     "phone": user.phone,
+    #                     "first_name": user.first_name,
+    #                     "last_name": user.last_name,
+    #                     "profile_photo": {
+    #                         "image_id": str(user.profile_photo.id),
+    #                         "category": user.profile_photo.category,
+    #                         "image_url": user.profile_photo.image_url
+    #                     } if user.profile_photo else None,
+    #                     "profile_photo_url": user.profile_photo_url if user.profile_photo_url else None,
+    #                     "primary_address": {
+    #                         "id": str(user.primary_address.id),
+    #                         "address_line1": user.primary_address.address_line1,
+    #                         "address_line2": user.primary_address.address_line2,
+    #                         "city": user.primary_address.city,
+    #                         "state": user.primary_address.state,
+    #                         "postal_code": user.primary_address.postal_code
+    #                     } if user.primary_address else None,
+    #                     "is_verified": user.is_verified,
+    #                     "created_at": user.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+    #                     "updated_at": user.updated_at.strftime('%Y-%m-%d %H:%M:%S')
+    #                 }
+
+    #         # Format cover image
+    #         cover_image_data = None
+    #         if "cover_image" in news and news["cover_image"]:
+    #             cover_image_obj = get_media_by_id(news["cover_image"])  # Fetch from MongoDB
+    #             if cover_image_obj:
+    #                 cover_image_data = {
+    #                     "image_id": str(cover_image_obj["_id"]),
+    #                     "category": cover_image_obj.get("category"),
+    #                     "image_name": cover_image_obj.get("image_name"),
+    #                     "image_url": cover_image_obj.get("image_url"),
+    #                 }
+
+    #         # Format category
+    #         news_category = None
+    #         if "category" in news and news["category"]:
+    #             category_obj = get_news_category_by_id(news["category"]) 
+    #             if category_obj:
+    #                 news_category = {
+    #                     "id": str(category_obj["_id"]),
+    #                     "title": category_obj.get("title"),
+    #                     "slug": category_obj.get("slug"),
+    #                     "description": category_obj.get("description"),
+    #                     "icon_url": category_obj.get("icon_url"),
+    #                     "priority": category_obj.get("priority"),
+    #                     "status": category_obj.get("status"),
+    #                     "is_featured": category_obj.get("is_featured"),
+    #                     "is_trending": category_obj.get("is_trending"),
+    #                     "keywords": category_obj.get("keywords"),
+    #                     "created_at": category_obj.get("created_at").strftime('%Y-%m-%d %H:%M:%S') if category_obj.get("created_at") else None,
+    #                     "updated_at": category_obj.get("updated_at").strftime('%Y-%m-%d %H:%M:%S') if category_obj.get("updated_at") else None,
+    #                 }
